59e5e9960f03ffc0c83a8b13387925fc85bde891
Steven Difelippo <matrixdriver04@gmail.com>
2017-09-24 16:06:31 -0400

    account info showing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/App.css';
-
-
 //import components and containers
-
 
 class App extends Component {
   render() {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
-import {combineReducers} from 'redux';
+// import {combineReducers} from 'redux';
 import {USER_SELECTED, ACCOUNT_SELECTED, WITHDRAW_FUNDS} from '../actions/index';
 import userList from '../data/users';
-import update from 'immutability-helper';
+// import update from 'immutability-helper';
+import _ from 'lodash';
 
 const initialState = {
     users: userList(),
@@ -10,39 +11,51 @@ const initialState = {
 }
 
 const reducer = function(state = initialState, action) {
+
+		let newState = _.cloneDeep(state);
     switch (action.type) {
-        case USER_SELECTED:
-            return update(state, {
-                selectedUser: {
-                    $set: action.payload
-                }
-            });
-        /*
+			case USER_SELECTED:
+				let userId = action.payload;
+				newState.selectedUser = newState.users.find(element => {
+					return element._id === userId;
+				});
+				return newState;
 
-          You will need to correct a reducer case for ACCOUNT_SELECTED here - HINT: it should mimic closely the USER_SELECTED case.
+			case ACCOUNT_SELECTED:
+				newState.selectedAccount = action.payload;
+				return newState;
+			/*
+				You will need to correct a reducer case for ACCOUNT_SELECTED here - HINT: it should mimic closely the USER_SELECTED case.
+			*/
+			case WITHDRAW_FUNDS:
+        newState = _.cloneDeep(state);
+        const account = newState.selectedAccount;
+        account.balance -= action.payload;
+        return newState;
+			// case WITHDRAW_FUNDS:
+			// 	// const userIdx = newState.users.findIndex(user => user._id === newState.selectedUser);
+			// 	// const accountIdx = newState.users[userIdx].accounts.findIndex(account => account.id === state.selectedAccount);
+			// 	const account = newState.accout.balance;
+			// 	newState.users[userIdx].accounts[accountIdx].balance -= action.payload;
+			// 	return newState;
 
-        */
-        case WITHDRAW_FUNDS:
-            const userIdx = state.users.findIndex(user => user._id === state.selectedUser);
-            const accountIdx = state.users[userIdx].accounts.findIndex(account => account.id === state.selectedAccount);
-
-            return update(state, {
-            users: {
-              [userIdx]: {
-                accounts: {
-                  [accountIdx]: {
-                    balance: {
-                      $apply: function(balance) {
-                        return balance - action.payload
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              })
-        default:
-            return state;
+					// return update(state, {      THIS IS FROM IMMUTABILITY HELPER - WE ARE USING LODASH INSTEAD
+					//     users: {
+					//         [userIdx]: {
+					//             accounts: {
+					//                 [accountIdx]: {
+					//                     balance: {
+					//                         $apply: function(balance) {
+					//                             return balance - action.payload
+					//                         }
+					//                     }
+					//                 }
+					//             }
+					//         }
+					//     }
+					// })
+			default:
+				return state;
     }
 }
 
